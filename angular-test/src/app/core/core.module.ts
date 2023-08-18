a255196e6cae08bc069255e1b8ad7a1f531652ab
Nebesnaya94 <kuroi.alice.13@gmail.com>
2023-08-18 07:28:32 +0300

    feat: add user button and router links in header

--- angular-test/src/app/core/core.module.ts
+++ angular-test/src/app/core/core.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { SharedModule } from '../shared/shared.module';
+import { AppRoutingModule } from '../app-routing.module';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, SharedModule, AppRoutingModule],
   exports: [HeaderComponent],
 })
 export class CoreModule {}
